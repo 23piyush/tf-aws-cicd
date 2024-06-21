457aab4022d3038e4fea4eb003269983d975fb52
Piyush Kumar <piyushk@vdi-bglddp-068.dhcp.mathworks.com>
2024-06-21 12:01:31 +0530

    formatted terraform files

--- src/main.tf
+++ src/main.tf
@@ -1,7 +1,7 @@
 terraform {
   # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
   # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
-   /*backend "s3" {
+  /*backend "s3" {
     bucket         = "cc-tf-state-backend-local-ci-cd"
     key            = "tf-infra/terraform.tfstate"
     region         = "us-east-1"
